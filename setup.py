52bdb72aa3d9563f2e7325306b0c0c2335e9267b
Artem Baguinski <artm@v2.nl>
2011-07-05 19:36:17 +0200

    drop the extension + setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dpgconv',
+	version='10.1',
+	description='Transcode video files to DPG format suitable for Nintendo DS',
+	author='Anton Romanov',
+	author_email='theli@ukr.net',
+	url='http://theli.is-a-geek.org/blog/static/dpgconv',
+	license='GNU General Public License v2.0',
+    scripts=['dpgconv']
+    )
+
